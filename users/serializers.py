35c4c1850f706001ca23fec92b1a32c067f86970
user <kilavyk@yandex.ru>
2025-08-12 20:31:25 +0300

    user registration

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from users.models import Payment
+from users.models import Payment, User
+
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+
+
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'city', 'avatar']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from users.views import PaymentListAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from users.apps import UsersConfig
+from users.views import (PaymentListAPIView, UserCreateAPIView,
+                         UserRetrieveAPIView, UserUpdateAPIView,
+                         UserDestroyAPIView)
+
+app_name = UsersConfig.name
 
 urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='payment-list'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user-detail'),
+    path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user-update'),
+    path('<int:pk>/delete/', UserDestroyAPIView.as_view(), name='user-delete'),
 ]
